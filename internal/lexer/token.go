4036e3cb4bb820343545690682b5711809638ab1
mel <mcmelon@nodiumhosting.com>
2026-08-30 17:16:37 +0200

    feat: reverse LUT for token literals

--- internal/lexer/token.go
+++ internal/lexer/token.go
@@ -55,7 +55,6 @@ const (
 	TOKEN_BITWISE_AND_ASSIGN // &=
 	TOKEN_BITWISE_OR_ASSIGN  // |=
 	TOKEN_BITWISE_XOR_ASSIGN // ^=
-	TOKEN_BITWISE_NOT_ASSIGN // ~=
 
 	// Comparison
 	TOKEN_EQUALS         // ==
@@ -130,7 +129,6 @@ var tokenLiterals = map[TokenKind]string{
 	TOKEN_BITWISE_AND_ASSIGN: "&=",
 	TOKEN_BITWISE_OR_ASSIGN:  "|=",
 	TOKEN_BITWISE_XOR_ASSIGN: "^=",
-	TOKEN_BITWISE_NOT_ASSIGN: "~=",
 
 	// Comparison
 	TOKEN_EQUALS:         "==",
@@ -169,4 +167,73 @@ var tokenLiterals = map[TokenKind]string{
 
 	// Reference
 	TOKEN_POUND: "#",
+}
+
+var literalTokens = map[string]TokenKind{
+	// Arithmetic
+	"+": TOKEN_PLUS,
+	"-": TOKEN_MINUS,
+	"*": TOKEN_MULTIPLY,
+	"/": TOKEN_DIVIDE,
+	"%": TOKEN_MODULO,
+
+	// Assignment
+	":=": TOKEN_DECLARE_ASSIGN,
+	"=":  TOKEN_ASSIGN,
+	"+=": TOKEN_PLUS_ASSIGN,
+	"-=": TOKEN_MINUS_ASSIGN,
+	"*=": TOKEN_MULTIPLY_ASSIGN,
+	"/=": TOKEN_DIVIDE_ASSIGN,
+	"%=": TOKEN_MODULO_ASSIGN,
+
+	// Bitwise operations
+	"&": TOKEN_BITWISE_AND,
+	"|": TOKEN_BITWISE_OR,
+	"^": TOKEN_BITWISE_XOR,
+	"~": TOKEN_BITWISE_NOT,
+
+	"<<": TOKEN_BITWISE_SHIFT_LEFT,
+	">>": TOKEN_BITWISE_SHIFT_RIGHT,
+
+	"&=": TOKEN_BITWISE_AND_ASSIGN,
+	"|=": TOKEN_BITWISE_OR_ASSIGN,
+	"^=": TOKEN_BITWISE_XOR_ASSIGN,
+
+	// Comparison
+	"==": TOKEN_EQUALS,
+	"!=": TOKEN_NOT_EQUALS,
+	">=": TOKEN_GREATER_EQUALS,
+	"<=": TOKEN_LESS_EQUALS,
+
+	"<": TOKEN_CHEVRON_LEFT,
+	">": TOKEN_CHEVRON_RIGHT,
+
+	// Delimiters
+	"(":   TOKEN_PAREN_OPEN,
+	")":   TOKEN_PAREN_CLOSE,
+	"{":   TOKEN_BRACE_OPEN,
+	"}":   TOKEN_BRACE_CLOSE,
+	"[":   TOKEN_BRACKET_OPEN,
+	"]":   TOKEN_BRACKET_CLOSE,
+	";":   TOKEN_SEMICOLON,
+	":":   TOKEN_COLON,
+	",":   TOKEN_COMMA,
+	".":   TOKEN_DOT,
+	"@":   TOKEN_AT,
+	"->":  TOKEN_ARROW_RIGHT,
+	"<-":  TOKEN_ARROW_LEFT,
+	"<->": TOKEN_ARROW_BI,
+
+	// Logic
+	"&&": TOKEN_LOGICAL_AND,
+	"||": TOKEN_LOGICAL_OR,
+
+	// Optional
+	"?":  TOKEN_QUESTION,
+	"!":  TOKEN_BANG,
+	"??": TOKEN_NONE_COALESCE,
+	":?": TOKEN_NONE_CONDITION,
+
+	// Reference
+	"#": TOKEN_POUND,
 }
